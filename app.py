f683042276cec29f78dd2105467e6fdc5738fdf8
heavensighs <121219095+heavensighs@users.noreply.github.com>
2024-12-09 07:30:42 +0000

    1

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 # Syn code spaces is very high risk, sometimes fail
 
 from flask import Flask, render_template, request
+import textblob
 
 app = Flask(__name__)
 
@@ -13,6 +14,18 @@ def index():
 def main():
     name = request.form.get("q")
     return render_template("main.html")
+
+@app.route("/SA", methods=["GET", "POST"])
+def SA():
+    name = request.form.get("q")
+    return render_template("SA.html")
+
+
+@app.route("/SA_result", methods=["GET", "POST"])
+def SA_result():
+    q = request.form.get("q")
+    r = textblob.TextBlob(q).sentiment
+    return render_template("SA_result.html",r=r)
 
 if __name__ == "__main__":
     app.run()      
